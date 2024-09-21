802a5020f9b3782259fb98f39669b096b5520508
yee <2399182941@qq.com>
2024-09-21 08:07:30 +0000

    update NetSecLab/urls.py.
    Signed-off-by: yee <2399182941@qq.com>

--- NetSecLab/urls.py
+++ NetSecLab/urls.py
@@ -19,4 +19,9 @@ from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.home, name='home'),  # 首页
+    path('login/', views.login_view, name='login'),  # 登录页面
+    path('competition/', views.competition, name='competition'),  # 竞赛公示
+    path('lab/', views.lab, name='lab'),  # 实验室公示
+    path('guide/', views.guide, name='guide'),  # 使用指南 
 ]
