a07fc63e55a4c57114071aa5e0779fcc192e807b
circle <2382374380@qq.com>
2024-09-21 17:28:23 +0800

    登录表单连接学生信息数据表

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 from django.shortcuts import render
@@ -7,7 +8,17 @@ def home(request):
     return render(request, 'home.html')
 
 def login_view(request):
-    return render(request, 'login.html')
+    stu_name = request.POST.get('username')
+    stu_password = request.POST.get('password')
+    if stu_name and stu_password:
+        # StudentInfo是学生资料数据表的名字，字段名为下面两个紫色的字符串
+        c = StudentInfo.objects.filter(stu_name=stu_name, stu_psw=stu_password).count()
+        if c == 1 :
+            return HttpResponse("登录成功")
+        else:
+            return HttpResponse("账号密码错误")
+    else:
+        return HttpResponse("账号密码不能为空")
 
 def competition(request):
     return render(request, 'competition.html')
