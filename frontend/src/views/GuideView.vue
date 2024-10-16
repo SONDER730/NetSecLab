8c00564cc38c22150a1cdb6ab91820d0a1bd2477
circle <2382374380@qq.com>
2024-10-16 12:55:26 +0800

    使用指南的分类

--- frontend/src/views/GuideView.vue
+++ frontend/src/views/GuideView.vue
@@ -1,24 +1,104 @@
 <template>
   <div class="guide-view">
-    <h1>使用指南</h1>
-    <p>这里是使用指南的内容。</p>
+
+      <!-- 左侧菜单 -->
+    <el-menu
+      class="el-menu-vertical"
+      :default-active="activeMenu"
+      @select="handleMenuSelect"
+      background-color="#001f3f"
+      text-color="#fff"
+      active-text-color="#409eff"
+    >
+      <el-menu-item index="signup_com">
+        如何报名
+      </el-menu-item>
+      <el-menu-item index="competitions">
+        查看竞赛信息
+      </el-menu-item>
+      <el-menu-item index="contact">
+        联系我们
+      </el-menu-item>
+    </el-menu>
+
+  <!-- 右侧内容区域 -->
+    <div class="content-container">
+      <div class="main">
+          <h1>使用指南</h1>
+      </div>
+
+
+      <!-- 竞赛报名部分 -->
+      <div v-if="activeMenu === 'signup_com'">
+        <div>
+          <p>这里是竞赛报名的步骤</p>
+        </div>
+      </div>
+
+       <!-- 查看实验室信息部分 -->
+      <div v-if="activeMenu === 'competitions'">
+        <div>
+          <p>这里是查看竞赛信息的步骤</p>
+        </div>
+      </div>
+
+      <!-- 联系我们信息部分 -->
+      <div v-if="activeMenu === 'contact'">
+        <div>
+          <p>这里是联系我们的步骤</p>
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
 
+
+
+
 <script>
 export default {
+  data(){
+    return{
+      activeMenu: 'signup_com',
+    }
+  },
+   methods: {
+    handleMenuSelect(index) {
+      this.activeMenu = index;
+    },
+  },
   name: 'GuideView',
 };
 </script>
 
 <style scoped>
 .guide-view {
-  text-align: center;
+  display: flex;
+  height: 100vh;
+  background-color: #001f3f;
+}
+
+/* 左侧菜单样式 */
+.el-menu-vertical {
+  width: 250px;
+  background-color: #001f3f;
+  color: #fff;
+}
+
+.content-container {
+  flex: 1;
   padding: 20px;
+  background-color: white;
+  color: black;
 }
 
 h1 {
   font-size: 2rem;
   color: #409EFF;
 }
+
+.main{
+
+ }
 </style>
